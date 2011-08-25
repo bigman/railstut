150d0c0f2f5c117aa68b142e951f63c0eb95861d
Vladimir Blagojevic <vladimir.blagojevic@gmail.com>
2011-08-25 21:43:32 +0200

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -22,4 +22,9 @@ describe "LayoutLinks" do
     response.should have_selector("title", :content=>"Help")
   end
 
+  it "Should have a Sign up page at /signup" do
+    get "/signup"
+    response.should have_selector("title", :content=>"Sign up")
+  end
+
 end
