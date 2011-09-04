2b41772faad938ce51dceb3addc673866ba08a06
bigman <vladimir.blagojevic@gmail.com>
2011-09-04 17:56:09 +0200

    user validation

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
+require File.dirname(__FILE__) + "/../lib/faster_require-0.9.2/lib/faster_require" # speed things up on Windows 
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
